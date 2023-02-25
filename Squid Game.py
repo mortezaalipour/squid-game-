f3962071f5f7c8cb6df9a10a8321ca7e66fbdfe9
Morteza Alipour <108768050+mortezaalipour@users.noreply.github.com>
2023-02-25 21:55:42 +0100

    Add files via upload

---
+++ Squid Game.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+
+# In[2]:
+
+
+data = pd.read_csv("tweets_v8.csv")
+print(data.head())
+
+
+# In[3]:
+
+
+print(data.isnull().sum())
+
+
+# In[10]:
+
+
+data = data.drop(columns="user_description", axis=1)
+
+
+# In[7]:
+
+
+data = data.dropna()
+
+
+# In[11]:
+
+
+print(data.isnull().sum())
+
+
+# In[12]:
+
+
+import nltk
+import re
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
+
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+data["text"] = data["text"].apply(clean)
+
+
+# In[13]:
+
+
+text = " ".join(i for i in data.text)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+
+# In[14]:
+
+
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["text"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["text"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["text"]]
+data = data[["text", "Positive", "Negative", "Neutral"]]
+print(data.head())
+
+
+# In[15]:
+
+
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😠 ")
+    else:
+        print("Neutral 🙂 ")
+sentiment_score(x, y, z)
+
+
+# In[16]:
+
+
+print("Positive: ", x)
+print("Negative: ", y)
+print("Neutral: ", z)
+
+
+# In[ ]:
+
+
+
+
